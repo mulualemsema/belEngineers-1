f985540303135b9eb231d4f057ec1357bc64f1a4
Asrat Minase <asrat461@revature.net>
2024-12-02 13:53:24 -0600

    add WeConfig to configure CORS

--- src/main/groovy/com/belengineers_tx/belengineers_tx/authenticate/UserController.groovy
+++ src/main/groovy/com/belengineers_tx/belengineers_tx/authenticate/UserController.groovy
@@ -1,6 +1,5 @@
 package com.belengineers_tx.belengineers_tx.authenticate
 
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = "http://localhost:3000")
 class UserController {
 
     private final UserService userService
