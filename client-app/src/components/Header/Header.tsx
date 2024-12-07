4610a101dfdffea9e365da11b62ce50311c79a50
Asrat Minase <asrat461@revature.net>
2024-12-07 12:24:26 -0600

    - refactor some of the code

--- client-app/src/components/Header/Header.tsx
+++ client-app/src/components/Header/Header.tsx
@@ -35,7 +35,7 @@ const Header: React.FC = () => {
                 <span className="phone-number">(512) 363-0461</span>
             </div>
 
-            <div className="header-container">
+            <div className="home-container">
                 <header className={`header ${scrolled ? 'scrolled' : ''}`}>
                     <div className="logo-container">
                         <img src="/logo.png"
