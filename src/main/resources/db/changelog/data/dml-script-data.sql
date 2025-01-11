a0c1ec04c76c819e5512086064569e1933cd0ad3
mulualemsema <demissie@belengineers.com>
2025-01-11 12:38:56 -0600

    update temporary password

--- src/main/resources/db/changelog/data/dml-script-data.sql
+++ src/main/resources/db/changelog/data/dml-script-data.sql
@@ -1,2 +1,2 @@
 INSERT INTO users (username, password, first_login, created_at, updated_at)
-VALUES ('dsema', '$2a$12$yO8Ok8A7R1U8IbYHizl3WupLWqsAPey.oMOm2UmuvTaxSt40eF78G', true, CURRENT_DATE, CURRENT_DATE);
+VALUES ('demissieSema', '$2a$12$l5kHSlr6C7wPqJayPcWyTeIV0/8eyEkoB.YV.f4LPuMxC/AxB.sMC', true, CURRENT_DATE, CURRENT_DATE);
